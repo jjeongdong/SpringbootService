9001df7f511c4c1ae4ac60fa389acea121d66ab5
jjeongdong <donghun3296@naver.com>
2023-04-30 15:30:06 +0900

    chore : Lombok 적용

---
+++ src/main/java/com/example/springbootwebservice/web/dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.springbootwebservice.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+
+}
